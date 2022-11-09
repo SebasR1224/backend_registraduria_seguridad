67a585dd20c5c5d91042092976150d2933057b4a
Sebastian <jr7815611@gmail.com>
2022-11-08 21:57:53 -0500

    first commit

---
+++ src/main/java/backend/security/security/BackendRegistraduriaSeguridadApplication.java
@@ -0,0 +1,13 @@
+package backend.security.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRegistraduriaSeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRegistraduriaSeguridadApplication.class, args);
+	}
+
+}
