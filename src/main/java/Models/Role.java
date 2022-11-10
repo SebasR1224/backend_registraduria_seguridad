26f04d6c9040ce5944836913c6957f1ba93fbed6
Sebastian <jr7815611@gmail.com>
2022-11-09 20:14:23 -0500

    Desarrollo de Modelos y Repositorios

---
+++ src/main/java/Models/Role.java
@@ -0,0 +1,30 @@
+package Models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document()
+public class Role {
+    @Id
+    private String _id;
+    private String name;
+    
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/Repositories/PermissionRepository.java
@@ -0,0 +1,9 @@
+package Repositories;
+
+import Models.Permission;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PermissionRepository extends MongoRepository<Permission, String>
+{
+
+}
